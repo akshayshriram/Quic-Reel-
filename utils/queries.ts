34ce5947f231c78b46ffe8e94359b9a5be4310c1
Akshay <akshayshriram999@gmail.com>
2024-06-11 21:26:38 +0530

    Added type any to the topicPostQuery query

--- utils/queries.ts
+++ utils/queries.ts
@@ -164,7 +164,7 @@ export const userLikedPostsQuery = (userId: string | string[]) => {
   return query;
 };
 
-export const topicPostsQuery = (topic: string | string[]) => {
+export const topicPostsQuery = (topic?: string | string[]) => {
   const query = `*[_type == "post" && topic match '${topic}*'] {
     _id,
      caption,
