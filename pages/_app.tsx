b075056875fc8f66f8e96c1e22a992d78f3aa201
Akshay <akshayshriram999@gmail.com>
2024-12-21 20:28:28 +0530

    Add CORS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,32 +2,50 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
 
-import { GoogleOAuthProvider, GoogleLogin, googleLogout } from '@react-oauth/google';
+import {
+  GoogleOAuthProvider,
+  GoogleLogin,
+  googleLogout,
+} from "@react-oauth/google";
 
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import Cors from "cors";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
 
   useEffect(() => {
     setIsSSR(false);
-  },[]);
+  }, []);
+
+  // Initialize the cors middleware
+  Cors({
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    origin: [
+      "https://quickreel.akshayshriram.in",
+      "https://quic-reel.vercel.app",
+    ],
+    credentials: true,
+  });
 
   return (
-  <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
-    <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
-      <Navbar/>
+    <GoogleOAuthProvider
+      clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
+    >
+      <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+        <Navbar />
         <div className="flex gap-6 md:gap-20">
           <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto xl:w-400">
-            <Sidebar/>
+            <Sidebar />
           </div>
           <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
             <Component {...pageProps} />
           </div>
         </div>
-    </div>
-  </GoogleOAuthProvider>)
-}
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
 
 export default MyApp;
